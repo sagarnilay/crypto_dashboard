1e0bdd18b0b84a559aed54f882f96df73ef1f814
sagarnilay <58388134+sagarnilay@users.noreply.github.com>
2021-11-17 23:59:00 +0530

    first commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+import Loader from './Loader';
+
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies } from '../components';
+
+const { Title } = Typography;
+
+const Homepage = () => {
+    
+    const { data, isFetching } = useGetCryptosQuery(10);
+   
+    const globalStats = data?.data?.stats;
+
+    if (isFetching) return <Loader />;
+
+
+    
+
+    return ( 
+        
+        <div className="background-home">
+            <Title level={2} className="heading">Global Crypto Statistics</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies"  value = {globalStats.total} /></Col> 
+                <Col span={12}><Statistic title="Total Exchanges"  value = {millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value = {millify(globalStats.totalMarketCap)} /></Col>
+                <Col span={12}><Statistic title="Total 24h volume" value = {millify(globalStats.total24hVolume)} /></Col>
+                <Col span={12}><Statistic title="Markets" value = {millify(globalStats.totalMarkets)} /></Col>
+                
+            </Row>
+            
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 25 Cryptos In The World</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+            
+
+        
+        </div>
+    );
+};
+
+export default Homepage;
